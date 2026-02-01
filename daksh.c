8290045539457d209d3427d1b01dba30731bb4c9
Rainbow Martin <Shannon.Martin@trojans.dsu.edu>
2026-02-01 15:22:33 -0600

    Added error message function and check for more than 2 arguments.

--- daksh.c
+++ daksh.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+void eMessage(void) {
+    char errorMessage[30] = "An error has occurred\n";
+    write(STDERR_FILENO, errorMessage, strlen(errorMessage));
+}
 
-int main(void) {
+int main(int argc, char* argv[]) {
+
+    // program invoked with more than 2 arguments
+    // exit program after error message
+    if (argc > 2) {
+        eMessage();
+        exit(1);
+    }
 
     // character array to hold input
     char input[50];
@@ -15,7 +28,10 @@ int main(void) {
         fgets(input, sizeof(input), stdin);
         input[strcspn(input, "\n")] = '\0';
 
+
+
     } while (strcmp(input, "exit")!= 0);
 
     return 0;
 }
+
