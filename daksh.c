5bb4970394188f1e7028e9d59d3527223e94d601
Rainbow Martin <Shannon.Martin@trojans.dsu.edu>
2026-02-01 18:55:01 -0600

    Started on cd command. Need to update prompt to display directory?

--- daksh.c
+++ daksh.c
@@ -9,43 +9,17 @@ void eMessage(void) {
     write(STDERR_FILENO, errorMessage, strlen(errorMessage));
 }
 
-char** breakString(char* inputString, int *count) {
-
-    // array of pointers to each word
-    char** words = NULL;
-    // pointer to individual word
-    char* word;
-    *count = 0;
-
-    // get the first word
-    word = strtok(inputString, " ");
-
-    // while there is another word waiting
-    while (word != NULL) {
-        int newCount = *count + 1;
-
-        //reallocate memory for an additional word's pointer
-        char** temporary = realloc(words, (newCount + 1) * sizeof(char*));
-        
-        // make the words array the newly allocated array
-        words = temporary;
-        // insert the word in the next open spot of the array
-        words[*count] = word;
-        // increment counter
-        (*count)++;
-
-        words[*count] = NULL;
-
-        // get the next word
-        word = strtok(NULL, " ");
-    }
-
-    return words;
-}
-
 int main(int argc, char* argv[]) {
 
     bool run = true;
+
+    int root;
+
+    root = chdir("/home/dsu");
+
+    if (root != 0) {
+        eMessage();
+    }
 
     // program invoked with more than 2 arguments
     // exit program after error message
@@ -80,27 +54,25 @@ int main(int argc, char* argv[]) {
             // replace newline with terminator
             input[length - 1] = '\0';
             char* word;
+            char* words[10];
 
             // break the string into tokens
-            //int count = 0;
-            //char** words = breakString(input, &count);
-            int i = 1;
+            int i = 0;
             while ((word = strsep(&input, " ")) != 0) {
-                printf("Token %d is %s\n", i, word);
+                words[i] = word;
                 i++;
             }
 
             // "exit" entered
-            //if (strcmp(words[0], "exit") == 0) {
-            //    exit(0);
-            //}
+            if (strcmp(words[0], "exit") == 0) {
+                exit(0);
+            }
 
-
-
-            //free(words);
+            // cd command
+            if (strcmp(words[0], "cd") == 0) {
+                chdir(words[1]);
+            }
         }
-
-
 
     } while (run);
 
