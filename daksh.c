31e024c13152cdc9576ad2f8c1c4502cd86acacd
Rainbow Martin <Shannon.Martin@trojans.dsu.edu>
2026-02-03 12:12:05 -0600

    Moved commands to separate function

--- daksh.c
+++ daksh.c
@@ -11,6 +11,113 @@ void eMessage(void) {
     write(STDERR_FILENO, errorMessage, strlen(errorMessage));
 }
 
+void exCommand(char* words[]) {
+    // "exit" entered
+    if (strcmp(words[0], "exit") == 0) {
+        exit(0);
+    }
+
+    // cd command
+    if (strcmp(words[0], "cd") == 0) {
+        // variables to store directories
+        char presentDirectory[60];
+        getcwd(presentDirectory, 60);
+
+        char previousDirectory[60];
+        const char *home = "/home";
+        char newDirectory[60];
+
+        // count arguments to cd
+        int c = 0;
+        for(int i = 1; i < 3; i++){
+            if (words[i] != NULL) {
+                c++;
+            }
+        }
+
+        // if there is not one argument, error
+        if (c != 1) {
+            eMessage();
+        }
+
+        else {
+            // argument does not equal "~", go to directory entered
+            if (strcmp(words[1], "~") != 0) {
+                strcpy(newDirectory, words[1]);
+
+                // make sure directory is valid
+                int working = chdir(newDirectory);
+                if( working != 0) {
+                    perror("chdir");
+                }
+                // store current directory
+                getcwd(presentDirectory, 60);
+            }
+            // arguemnt is "~" and redirects to home
+            else if (strcmp(words[1], "~") == 0) {
+                strcpy(newDirectory, "/home");
+                chdir(newDirectory);
+                getcwd(presentDirectory, 60);
+            }
+        }
+        
+    }
+
+    // path command
+    else if (strcmp(words[0], "path") == 0) {
+        char* paths[20];
+
+        // count arguments to path
+        int c = 0;
+        for(int i = 1; i < 10; i++){
+            if (words[i] != NULL) {
+                c++;
+            }
+        }
+
+        if(c == 1 && strcmp(words[1], "clear") == 0) {
+            // clear the path
+            setenv("PATH", "", 1);
+        }
+
+        else {
+            for (int i = 1; words[i] != NULL; i++) {
+                char* presentPath = getenv("PATH");
+                char path[50];
+                strcpy(path, words[i]);
+                char* newPath;
+            
+                if (strcmp(presentPath, "") != 0) {
+                    size_t len = strlen(presentPath) + 1 + strlen(path) + 1;
+                    newPath = malloc(len);
+                    snprintf(newPath, len, "%s:%s", presentPath, path);
+                    setenv("PATH", newPath, 1);
+                    free(newPath);
+                }
+            }
+        }
+        printf("%s\n", getenv("PATH"));
+    }
+
+    // external commands
+    else {
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            eMessage();
+        }
+        else if (pid == 0) {
+            execvp(words[0], words);
+
+            exit(0);
+        }
+        else {
+            wait(NULL);
+        }
+
+    }
+}
+
 int main(int argc, char* argv[]) {
 
     bool run = true;
@@ -70,110 +177,7 @@ int main(int argc, char* argv[]) {
                 i++;
             }
 
-            // "exit" entered
-            if (strcmp(words[0], "exit") == 0) {
-                exit(0);
-            }
-
-            // cd command
-            if (strcmp(words[0], "cd") == 0) {
-                // variables to store directories
-                char presentDirectory[60];
-                getcwd(presentDirectory, 60);
-
-                char previousDirectory[60];
-                const char *home = "/home";
-                char newDirectory[60];
-
-                // count arguments to cd
-                int c = 0;
-                for(int i = 1; i < 3; i++){
-                    if (words[i] != NULL) {
-                        c++;
-                    }
-                }
-
-                // if there is not one argument, error
-                if (c != 1) {
-                    eMessage();
-                }
-
-                else {
-                    // argument does not equal "~", go to directory entered
-                    if (strcmp(words[1], "~") != 0) {
-                        strcpy(newDirectory, words[1]);
-
-                        // make sure directory is valid
-                        int working = chdir(newDirectory);
-                        if( working != 0) {
-                            perror("chdir");
-                        }
-                        // store current directory
-                        getcwd(presentDirectory, 60);
-                    }
-                    // arguemnt is "~" and redirects to home
-                    else if (strcmp(words[1], "~") == 0) {
-                        strcpy(newDirectory, "/home");
-                        chdir(newDirectory);
-                        getcwd(presentDirectory, 60);
-                    }
-                }
-                
-            }
-
-            // path command
-            else if (strcmp(words[0], "path") == 0) {
-                char* paths[20];
-
-                // count arguments to path
-                int c = 0;
-                for(int i = 1; i < 10; i++){
-                    if (words[i] != NULL) {
-                        c++;
-                    }
-                }
-
-                if(c == 1 && strcmp(words[1], "clear") == 0) {
-                    // clear the path
-                    setenv("PATH", "", 1);
-                }
-
-                else {
-                    for (int i = 1; words[i] != NULL; i++) {
-                        char* presentPath = getenv("PATH");
-                        char path[50];
-                        strcpy(path, words[i]);
-                        char* newPath;
-                    
-                        if (strcmp(presentPath, "") != 0) {
-                            size_t len = strlen(presentPath) + 1 + strlen(path) + 1;
-                            newPath = malloc(len);
-                            snprintf(newPath, len, "%s:%s", presentPath, path);
-                            setenv("PATH", newPath, 1);
-                            free(newPath);
-                        }
-                    }
-                }
-                printf("%s\n", getenv("PATH"));
-            }
-
-            // external commands
-            else {
-                pid_t pid = fork();
-
-                if (pid == -1) {
-                    eMessage();
-                }
-                else if (pid == 0) {
-                    execvp(words[0], words);
-
-                    exit(0);
-                }
-                else {
-                    wait(NULL);
-                }
-
-            }
+            exCommand(words);
 
             for(int j = 0; j < 10; j++) {
                 words[j] = '\0';
