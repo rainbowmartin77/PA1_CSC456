1198b9082666456971f2cdc85c8bb57744243511
Rainbow Martin <Shannon.Martin@trojans.dsu.edu>
2026-02-04 11:12:58 -0600

    Corrected path command logic

--- daksh.c
+++ daksh.c
@@ -7,16 +7,95 @@
 #include <sys/wait.h>
 
 // only error message for entire program
+void eMessage(void);
+
+void clearWords(char** words);
+
+void exCommand(char* words[]);
+
+void breakString(char** words, char* input, ssize_t length);
+
+int main(int argc, char* argv[]) {
+    // set initial path to /bin
+    setenv("PATH", "/bin", 1);
+
+    bool run = true;
+
+    // program invoked with more than 2 arguments
+    // exit program after error message
+    if (argc > 2) {
+        eMessage();
+        exit(1);
+    }
+
+    do {
+        char *input = NULL;
+        size_t capacity = 0;
+        ssize_t length = 0;
+        char* words[10];
+
+        // if file entered as argument, enter batch mode
+        if (argc == 2) {
+
+            FILE *file = fopen(argv[1], "r");
+
+            while ((length = getline(&input, &capacity, file))!= -1) {
+
+                // read input
+                breakString(words, input, length);
+                words[length] = NULL;
+
+                // next line exists
+                // execute command
+                exCommand(words);
+
+                clearWords(words);   
+            }
+
+            fclose(file);
+            exit(0);
+
+            free(input);
+        }
+
+        // no file passed in, enter interactive mode
+        else if (argc == 1) {
+            // print prompt
+            printf("daksh> ");
+
+            // read input
+            length = getline(&input, &capacity, stdin);
+            breakString(words, input, length);
+
+            exCommand(words);
+
+            clearWords(words);
+        }
+
+    } while (run);
+
+    return 0;
+}
+
 void eMessage(void) {
     char errorMessage[30] = "An error has occurred\n";
     write(STDERR_FILENO, errorMessage, strlen(errorMessage));
 }
 
-void clearWords(char** words) {
-    // clear words array
-    for(int x = 0; words[x] != NULL; x++) {
-        words[x] = NULL;
+void breakString(char** words, char* input, ssize_t length) {
+    // replace newline with terminator
+    input[length - 1] = '\0';
+    char* word;
+
+    // break the string into tokens
+    int i = 0;
+    while ((word = strsep(&input, " ")) != 0) {
+        words[i] = word;
+        i++;
     }
+    // make sure list of words in null terminated
+    words[i+1] = NULL;
+    
     return;
 }
 
@@ -104,6 +183,13 @@ void exCommand(char* words[]) {
                     setenv("PATH", newPath, 1);
                     free(newPath);
                 }
+                else {
+                    size_t len = strlen(presentPath) + 1 + strlen(path) + 1;
+                    newPath = malloc(len);
+                    snprintf(newPath, len, "%s", path);
+                    setenv("PATH", newPath, 1);
+                    free(newPath);
+                }
             }
         }
     }
@@ -119,98 +205,22 @@ void exCommand(char* words[]) {
             execvp(words[0], words);
 
             eMessage();
-
-            exit(0);
+            _exit(0);
         }
         else {
             wait(NULL);
-        }
-
-    }
-
-    clearWords(words);
-
-    return;
-}
-
-void breakString(char** words, char* input, ssize_t length) {
-    // replace newline with terminator
-    input[length - 1] = '\0';
-    char* word;
-
-    // break the string into tokens
-    int i = 0;
-    while ((word = strsep(&input, " ")) != 0) {
-        words[i] = word;
-        i++;
-    }
-    // make sure list of words in null terminated
-    words[i+1] = NULL;
-    
-    return;
-}
-
-int main(int argc, char* argv[]) {
-    setenv("PATH", "/bin", 1);
-
-    bool run = true;
-
-    // program invoked with more than 2 arguments
-    // exit program after error message
-    if (argc > 2) {
-        eMessage();
-        exit(1);
-    }
-
-    do {
-        char *input = NULL;
-        size_t capacity = 0;
-        ssize_t length;
-        char* words[10];
-
-        // if file entered as argument, enter batch mode
-        if (argc == 2) {
-
-            FILE *file = fopen(argv[1], "r");
-
-            do {
-                // read input
-                length = getline(&input, &capacity, file);
-                breakString(words, input, length);
-
-                // reached EOF
-                if(length == -1) {
-                    clearWords(words);
-                    exit(0);
-                }
-
-                // next line exists
-                // execute command
-                exCommand(words);
-                clearWords(words);
-
-            } while (length != -1);
-
-            free(input);
-            fclose(file);
-        }
-
-        // no file passed in, enter interactive mode
-        else if (argc == 1) {
-            // print prompt
-            printf("daksh> ");
-
-            // read input
-            length = getline(&input, &capacity, stdin);
-            breakString(words, input, length);
-
-            exCommand(words);
-
             clearWords(words);
         }
 
-    } while (run);
+    }
 
-    return 0;
+    return;
 }
 
+void clearWords(char** words) {
+    // clear words array
+    for(int x = 0; words[x] != NULL; x++) {
+        words[x] = NULL;
+    }
+    return;
+}
